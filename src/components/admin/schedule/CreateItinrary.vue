be015306361f709183f3be5bb197313d0056d845
DafneXCA <cruzdafne123@gmail.com>
2024-02-07 19:31:51 -0400

    Itineraries dinamic Area

--- src/components/admin/schedule/CreateItinrary.vue
+++ src/components/admin/schedule/CreateItinrary.vue
@@ -1,43 +1,120 @@
 <script setup>
-let dialog=false;
+import { useForm ,useField} from "vee-validate";
+import { onMounted ,ref} from "vue";
+import * as yup from 'yup';
+
+import { useItineraryStore } from '@/stores/admin/schedule/itineraryStore';
+import { useCareerStore } from '@/stores/admin/configgeneral/careerStore';
+
+
+//def Stores
+const itineraryStore=useItineraryStore();
+const careerStore=useCareerStore();
+
+
+//def props and emits
+const props = defineProps(['dialog','area']);
+const emit=defineEmits(['close-dialog']);
+
+
+
+//validation
+const schema = yup.object().shape({
+    nombre: yup.string().required(),
+    carrera: yup.string().required()
+});
+
+const {handleSubmit,resetForm}=useForm({
+    validationSchema: schema
+});
+
+const name=useField('nombre',schema);
+const career=useField('carrera',schema);
+
+
+const onSubmit=handleSubmit(
+    async (values,{resetForm}) =>{
+        const itinerary={
+            name:values.nombre,
+            careerId:values.carrera
+        }
+        await itineraryStore.saveItinerary(itinerary);
+        
+       resetForm();
+       emit('close-dialog');
+    }
+);
+
+const closeDialog=()=>{
+    resetForm();
+    emit('close-dialog');
+};
+
+onMounted(async ()=>{
+   
+    if(props.area){
+        
+        await careerStore.getCareers(props.area);
+    
+    }
+    
+});
+
 </script>
+
 <template>
-    <v-row justify="center">
-        <v-dialog v-model="dialog" persistent width="1024">
-            <template v-slot:activator="{ props }">
-                <v-btn color="primary" v-bind="props"> Open Dialog </v-btn>
-            </template>
-            <v-card>
-                <v-card-title>
-                <span class="text-h5">Añadir Itinerario</span>
-                </v-card-title>
 
-                <v-card-text>
+   <v-dialog v-model="props.dialog" persistent width="40vw"  >
+    
+        
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Añadir Itinerario</span>
+      </v-card-title>
+      <v-form @submit.prevent="onSubmit" >
+      <v-card-text>
+        <v-container>
 
-                <v-container>
-                    <v-row>
-                    <v-col cols="12">
-                        <v-text-field label="Nombre*" required></v-text-field>
-                    </v-col>
+     
+              
+              <v-text-field 
+              label="Nombre*" 
+              v-model="name.value.value"
+              :error-messages="name.errorMessage.value"
+              >
+            </v-text-field>
+           
 
-                    <v-col cols="12">
-                        <v-text-field label="Carrera*" required></v-text-field>
-                    </v-col>
-                    </v-row>
-                </v-container>
+          
+              <v-select
+              :items="careerStore.careers"
+              label="Carrera*"
+              :item-title="'name'"
+              :item-value="'id'"
+              required
+              v-model="career.value.value"
+              :error-messages="career.errorMessage.value"
+              ></v-select>
+           
 
-                </v-card-text>
+           
+            
 
-                <v-card-actions>
+       
+        </v-container>
+      </v-card-text>
+            <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
-                    Close
+                <v-btn color="blue-darken-1" variant="text" @click="closeDialog">
+                Cancelar
                 </v-btn>
-                <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
-                    Save
+                <v-btn type="submit" color="blue-darken-1" variant="text" >
+                Guardar
                 </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-  </v-row>
+            
+            </v-card-actions>
+        </v-form>
+    </v-card>
+    </v-dialog>
+
 </template>
--- src/stores/admin/schedule/itineraryStore.js
+++ src/stores/admin/schedule/itineraryStore.js
@@ -1,12 +1,24 @@
-import { ref  } from 'vue'
+import { ref ,watch } from 'vue'
 import { defineStore } from 'pinia'
 import AxiosAM from '@/services/AxiosAM.js'
+import { useMainStore } from '@/stores/MainStore';
 
-const pathItineraryResource="/admin/areas/1/itineraries";
+const mainStore = useMainStore();
+
+let pathItineraryResource=`/admin/areas/-1/itineraries`;
+
 
 export const useItineraryStore =defineStore('itineraryStore',()=>{
     
+  watch(() => mainStore.area.areaId, async (newAreaId) => {
+    if (newAreaId !== null) {
+      pathItineraryResource=`/admin/areas/${newAreaId}/itineraries`;
+    }
+  });
+
+
     const itineraries = ref([]);
+    const currentItinerary=ref(null);
 
   async function getItineraries() {
     try {
@@ -15,7 +27,6 @@ export const useItineraryStore =defineStore('itineraryStore',()=>{
         itineraries.value = data;
       }
     } catch (error) {
-      // handle error
       console.log('error getting itineraries');
     }
   }
@@ -25,11 +36,56 @@ export const useItineraryStore =defineStore('itineraryStore',()=>{
   async function saveItinerary(itinerary) {
     try {
       const { status, data } = await AxiosAM.post(pathItineraryResource,itinerary);
-    
+
+
+      if(status===201){
+        itineraries.value.push(data);
+      }
     } catch (error) {
       console.log('error save itinerary');
     }
   }
 
-   return { itineraries, getItineraries,saveItinerary }
+
+  async function getItineraryById(idItinerary) {
+    try {
+      const { status, data } = await AxiosAM.get(pathItineraryResource+"/"+idItinerary);
+      
+      if(status===200){
+        currentItinerary.value=data;
+      }
+
+    } catch (error) {
+      console.log('error get itinerary');
+    }
+  }
+
+  async function putItinerary(editItinerary) {
+    try {
+      const { status, data } = await AxiosAM.put(pathItineraryResource+"/"+currentItinerary.value.id,editItinerary);
+      
+      if(status===200){
+        this.getItineraries();
+      }
+
+    } catch (error) {
+      console.log('error save itinerary');
+    }
+  }
+
+  async function deleteItinerary(idItinerary) {
+    try {
+      const { status, data } = await AxiosAM.delete(pathItineraryResource+"/"+idItinerary);
+     
+      if(status===204){
+        this.getItineraries();
+      }
+
+    } catch (error) {
+      console.log('error delete itinerary');
+    }
+  }
+
+   return { itineraries,currentItinerary, getItineraries,saveItinerary,getItineraryById,putItinerary,deleteItinerary}
 })
+
