9861534b9187755e8ac6d1b5fa2e6f97e6da3787
JessRod2000 <90485625+JessRod2000@users.noreply.github.com>
2024-04-25 18:34:50 -0400

    Revert "Dev schedule UI"

--- src/views/admin/configgeneral/ScheduleParamsView.vue
+++ src/views/admin/configgeneral/ScheduleParamsView.vue
@@ -1,49 +1,12 @@
-<template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <HorarioForm/>
-      </v-container>
-    </v-main>
-  </v-app>
-</template>
-  
-<script>
-import { useMainStore } from '@/stores/MainStore'
-import HorarioForm from '../../../components/admin/schedule/SheduleForm.vue'
+<script setup>
 
-export default {
-  name: 'MainHorario',
-  components: {
-    HorarioForm
-  },
-
-  setup() {
-    const mainStore = useMainStore()
-
-    return {
-      areaData: mainStore.area
-    }
-  },
-
-  data() {
-    return {
-      componentId: null
-    }
-  },
-  methods: {
-    changeId(nuevo) {
-      this.componentId = toString(nuevo)
-    }
-  },
-
-  watch: {
-    areaData: {
-      handler: function (nuevo, oldVal) {
-        this.changeId(nuevo.areaId)
-      },
-      deep: true
-    }
-  }
-}
 </script>
+
+<template>
+  <h3>Parametros de Horario por factultad</h3>
+  <h3>{{globalAreaSelected}}</h3>
+</template>
+
+<style scoped>
+
+</style>
